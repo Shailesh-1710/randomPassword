bea89fe6244c5ac8c0082a26eeb240ed6e980876
Shailesh <shailupatel17@gmail.comm>
2024-01-27 15:21:11 +1000

    UI updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,73 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
+  const [length, setlength] = useState(8);
+  const [password, setpassword] = useState("");
+  const [numbersAllowed, setNumbersAllowed] = useState(false);
+  const [symbolsAllowed, setsymbolsAllowed] = useState(false);
   return (
-    <div className="bg-gray-800">
-      <h1>HELLO </h1>
-    </div>
+    <>
+      <div className="w-full max-w-screen-sm mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500'">
+        <h1 className="text-white text-center my-3">Password Ganerator</h1>
+        <div className="flex shadow rounded-lg overflow-hidden mb-4">
+          <input
+            type="text"
+            value={password}
+            className="outline-none w-full py-1 px-3"
+            placeholder="Password"
+            readOnly
+          />
+          <button className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0">
+            copy
+          </button>
+        </div>
+
+        <div className="flex text-sm gap-x-2">
+          <div className="flex items-center gap-x-1">
+            <input
+              type="range"
+              className="cursor-pointer"
+              min={7}
+              max={20}
+              value={length}
+              onChange={(e) => setlength(e.target.value)}
+            ></input>
+            <label className="text-orange-500 text-xs italic" htmlFor="length">
+              Lenght :{length}
+            </label>
+          </div>
+
+          <div className="flex items-center gap-x-1">
+            <input
+              type="checkbox"
+              defaultChecked={numbersAllowed}
+              onChange={() => {
+                setNumbersAllowed((prev) => !prev);
+              }}
+            ></input>
+            <label
+              className="text-orange-500 text-xs italic"
+              htmlFor="Numbers Allowed?"
+            >
+              Numbers Allowed?
+            </label>
+            <input
+              type="checkbox"
+              defaultChecked={symbolsAllowed}
+              onChange={() => {
+                setsymbolsAllowed((prev) => !prev);
+              }}
+            ></input>
+            <label
+              className="text-orange-500 text-xs italic"
+              htmlFor="Symbols Allowed?"
+            >
+              Symbols Allowed?
+            </label>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
