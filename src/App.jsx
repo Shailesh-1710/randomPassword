6fab29e082ed25d0f5b3409aff7f25f88c7c4d02
Shailesh <shailupatel17@gmail.comm>
2024-01-27 16:58:33 +1000

    Copy button functionality added.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import { useState, useCallback, useEffect } from "react";
+import { useState, useCallback, useEffect, useRef } from "react";
 import "./App.css";
 
 function App() {
   const [length, setlength] = useState(8);
   const [password, setpassword] = useState("");
-  const [numbersAllowed, setNumbersAllowed] = useState(false);
-  const [symbolsAllowed, setsymbolsAllowed] = useState(false);
+  const [numbersAllowed, setNumbersAllowed] = useState(true);
+  const [symbolsAllowed, setsymbolsAllowed] = useState(true);
+
+  const passwordRef = useRef(null);
 
   const ganeratePassword = useCallback(() => {
     let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
@@ -14,12 +16,17 @@ function App() {
     if (numbersAllowed) str += "0123456789";
     if (symbolsAllowed) str += "!@#$%^&*()-_=+";
 
-    for (let i = 1; i < length; i++) {
+    for (let i = 1; i <= length; i++) {
       let char_index = Math.floor(Math.random() * str.length + 1);
       pass += str.charAt(char_index);
     }
     setpassword(pass);
   });
+
+  const copyPasswordToClipboard = () => {
+    window.navigator.clipboard.writeText(password);
+    passwordRef.current?.select();
+  };
 
   useEffect(() => {
     ganeratePassword();
@@ -27,7 +34,7 @@ function App() {
 
   return (
     <>
-      <div className="w-full max-w-screen-sm mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500'">
+      <div className="w-full max-w-screen-lg mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500">
         <h1 className="text-white text-center my-3">Password Ganerator</h1>
         <div className="flex shadow rounded-lg overflow-hidden mb-4">
           <input
@@ -36,8 +43,12 @@ function App() {
             className="outline-none w-full py-1 px-3"
             placeholder="Password"
             readOnly
+            ref={passwordRef}
           />
-          <button className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0">
+          <button
+            className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0"
+            onClick={copyPasswordToClipboard}
+          >
             copy
           </button>
         </div>
@@ -53,7 +64,7 @@ function App() {
               onChange={(e) => setlength(e.target.value)}
             ></input>
             <label className="text-orange-500 text-xs italic" htmlFor="length">
-              Lenght :{length}
+              Length :{length}
             </label>
           </div>
 
